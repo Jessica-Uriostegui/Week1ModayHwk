1750c6787e6a12fef3259e63ce966171a4f8b2e0
J3ss25 <jessab.9@gmail.com>
2024-06-10 16:44:23 -0500

    First initial

---
+++ Hwk1.py
@@ -0,0 +1,41 @@
+# Lesson 1 Assignment
+# Jessica Uriostegui  June 10,2024
+
+# Assignment | Python Syntax
+# Task 1 Code correction
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
+
+
+#Task 2 Your mood today
+mood = input("How do you feel today? happy or sad? ")
+
+if mood == "happy":
+    print("That's great to hear!")
+if mood == "sad":
+    print("I hope your day gets better!")
+
+# Python Data Types and type() Function
+# Task 1 Code Correction
+variable_a = "Hello, World!"
+print(type(variable_a))
+variable_b = 23
+print(type(variable_b))
+variable_c = 3.14
+print(type(variable_c))
+variable_d = True
+print(type(variable_d))
+
+
+#Arithmetic Operations in Daily Life
+#Task 1 Grocery Store Math
+bread = 2.75
+peanut_butter= 4.50
+jelly = 3.23
+
+total_cost = float(bread + peanut_butter + jelly)
+print("The total cost is", total_cost)
